88521ea6b2ef3efaf415e45349c21f14dfe8ffc0
migimasa <migimasa@gmail.com>
2015-08-16 10:42:11 -0500

    Add Files

---
+++ MigiFramework/Sql/SqlClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Migi.Framework.Sql
+{
+    public class SqlClient
+    {
+        public static SqlParameter GetSqlParameter(string parameterName, SqlDbType dbType, object value)
+        {
+            SqlParameter param = new SqlParameter(parameterName, dbType);
+            param.Value = value;
+
+            return param;
+        }
+    }
+}
---
+++ MigiFrameworkHelper/Types.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Migi.Framework.Helper
+{
+    public class Types
+    {
+        public static Guid? GetNullableGuid(object value)
+        {
+            if (IsGuid(value))
+            {
+                return new Guid(value.ToString());
+            }
+            return null;
+        }
+
+        public static bool IsGuid(object value)
+        {
+            if (value != DBNull.Value && value != null)
+            {
+                Guid tryData;
+                if (Guid.TryParse(value.ToString(), out tryData))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
