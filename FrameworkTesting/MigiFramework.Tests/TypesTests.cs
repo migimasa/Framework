891ff88df6402ff53cd93472b689255b6d40a088
migimasa <migimasa@gmail.com>
2015-08-25 20:46:08 -0500

    Add Unit Testing

---
+++ FrameworkTesting/MigiFramework.Tests/TypesTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using Migi.Framework.Helper;
+
+namespace MigiFramework.Tests
+{
+    [TestFixture]
+    public class TypesTests
+    {
+        [TestCase("fc5f462d-84ba-43af-8fe7-cd036a3cfb76")]
+        [TestCase("FC5F462D-84BA-43AF-8fE7-CD036A3CFB76")]
+        public void IsGuid_ValidInput_ReturnsTrue(object value)
+        {
+            bool isValid = Types.IsGuid(value);
+
+            Assert.True(isValid);
+        }
+
+        [TestCase("test")]
+        [TestCase('c')]
+        [TestCase(12)]
+        [TestCase(true)]
+        [TestCase(12.43)]
+        public void IsGuid_InvalidInput_ReturnsFalse(object value)
+        {
+            bool isValid = Types.IsGuid(value);
+
+            Assert.False(isValid);
+        }
+    }
+}
---
+++ MigiFramework/Sql/IAccessLayerBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Migi.Framework.Sql
+{
+    public interface IAccessLayerBase
+    {
+        string ConnectionString { get; }
+        SqlConnection GetOpenConnection();
+    }
+}
